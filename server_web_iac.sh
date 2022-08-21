11f248962e96f87cd226db2dea386f694f7d3f7e
juniomsilva <junio.mosilva@gmail.com>
2022-08-21 00:24:43 -0300

    Correção script IAC para criação/configuração do servidor web - Versão 2.0

--- server_web_iac.sh
+++ server_web_iac.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
 echo "Atualizando Linux"
-
 apt update
 apt upgrade -y
-
-
 echo "Linux Atualizado"
 
 echo "Instalando Apache2"
-
 apt install apache2 -y
-
 echo "Apache2 Instalado!"
 
 echo "Instalando Unzip"
-
 apt install unzip -y
-
 echo "Unzip Instalado!"
 
 echo "Baixar aplicação"
-
 cd /tmp/
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip 
-
 echo "Aplicação baixada"
 
 echo "Copiando aplicação"
-
-cd /tmp/
-cp main.zip /var/www/html
-cd /var/www/html
 unzip main.zip
-
+cd linux-site-dio-main
+cp -R * /var/www/html/
+cd /var/www/html
 echo "Aplicação copiada"
+
+echo "Apagando arquivos temporários"
+cd /tmp/
+rm -Rf main.zip
+rm -Rf linux-site-dio-main
+ls -lh
+echo "Arquivos apagados"
+
+echo "Serviro Web Configurado e site no ar"
